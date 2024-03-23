e1e3bb3b7ca76bae64e6a415e9d8e7573d23dd0c
Azubuike Okom <azubuikeokom@gmail.com>
2024-03-23 12:40:09 +0100

    comment our engine version

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   cluster_identifier = aws_rds_cluster.default.id
   instance_class     = var.instance_class
   engine             = aws_rds_cluster.default.engine
-  # engine_version     = aws_rds_cluster.default.engine_version
+  engine_version     = aws_rds_cluster.default.engine_version
 }
 
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = var.cluster_identifier
   engine                  = var.engine 
-  # engine_version          = var.engine_version 
+  engine_version          = var.engine_version 
   availability_zones      = var.availability_zones
   database_name           = var.database_name
   master_username         = var.master_username
