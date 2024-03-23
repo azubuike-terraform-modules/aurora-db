16f803eb54151a7910a9414ee8674eb2c8a62bb8
Azubuike Okom <azubuikeokom@gmail.com>
2024-03-23 12:11:39 +0100

    Add terraform aurora basic config

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = var.count
+  identifier         = "${var.cluster_identifier}-${count.index}"
+  cluster_identifier = aws_rds_cluster.default.id
+  instance_class     = var.instance_class
+  engine             = aws_rds_cluster.default.engine
+  engine_version     = aws_rds_cluster.default.engine_version
+}
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = var.cluster_identifier
+  engine                  = var.engine 
+  engine_version          = var.engine_version 
+  availability_zones      = var.availability_zones
+  database_name           = var.database_name
+  master_username         = var.master_username
+  master_password         = var.master_password
+  backup_retention_period = var.backup_retention_period
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "instance_class" {
+    type = string
+}
+variable "cluster_identifier" {
+    type = string
+}
+variable "engine" {
+    type = string
+}
+variable "engine_version" {
+    type = string
+}
+variable "availability_zones" {
+    type = list(string)
+}
+variable "database_name" {
+    type = string
+}
+variable "master_username" {
+    type = string
+}
+variable "master_password" {
+    type = string
+}
+variable "backup_retention_period" {
+    type = number
+}
+variable "count" {
+  type = number
+}
