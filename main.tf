f7c052cfbe39cfc57da0cfab8626c5092a42f785
Azubuike Okom <azubuikeokom@gmail.com>
2024-03-23 12:36:53 +0100

    comment our engine version

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = var.cluster_identifier
   engine                  = var.engine 
-  engine_version          = var.engine_version 
+  # engine_version          = var.engine_version 
   availability_zones      = var.availability_zones
   database_name           = var.database_name
   master_username         = var.master_username
